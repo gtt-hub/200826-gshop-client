7d8b07acfe7e4ce6e98b5e20a011a3b629d3d0d9
gutong <2633445061@qq.com>
2020-12-22 16:58:43 +0800

    update

---
+++ src/pages/Search/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Search</p>
+    <h2>params.keyword: {{$route.params.keyword}}</h2>
+    <h3>query.keyword: {{$route.query.keyword2}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+}
+</script>
+
+<style scoped>
+
+</style>
