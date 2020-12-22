7d55d77cd2013d3f11de587326ed36e701345a12
gutong <2633445061@qq.com>
2020-12-22 11:05:07 +0800

    update README

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+// module.exports = {
+//   //5个
+//   // entry:'',
+//   // outPut:{
+//   // },
+//   // module:{
+//   //   rules:[]
+//   // },
+//   // plugins:[],
+//   // mode:''
+
+//   //禁用eslint
+//   lintOnSave: false
+// }
+
+module.exports = {
+  //禁用eslint
+  lintOnSave: false
+}
